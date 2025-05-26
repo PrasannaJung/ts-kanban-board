88d1fefda12c352dae0eded483116515413d1b90
PrasannaJung <prasannajung08@gmail.com>
2025-05-26 11:16:09 +0545

    Add drag drop functionality with restriction to move one board at a time

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,35 @@
 "use strict";
-const boards = ["Backlog", "Doing", "Review", "Done"];
-const doingBox = document.querySelector(".doing");
+const boards = ["Todo", "Doing", "Done"];
+const allBoards = document.querySelectorAll(".board");
 const allCards = document.querySelectorAll(".card");
 allCards.forEach((card) => {
-    card.addEventListener("dragstart", function (event) {
+    //   card.addEventListener("dragstart", function (event: DragEvent) {
+    //     const targetElement = event.target as HTMLDivElement;
+    //     console.log("THE ELEMENT BEING DRAGGED IS ", targetElement.id);
+    //     event.dataTransfer?.setData("text/plain", targetElement.id);
+    //   });
+});
+allBoards.forEach(function (board) {
+    board.addEventListener("dragstart", function (event) {
         var _a;
         const targetElement = event.target;
         console.log("THE ELEMENT BEING DRAGGED IS ", targetElement.id);
-        (_a = event.dataTransfer) === null || _a === void 0 ? void 0 : _a.setData("text/plain", targetElement.id);
+        const dto = {
+            boardId: parseInt(event.currentTarget.id),
+            targetElementId: targetElement.id,
+        };
+        (_a = event.dataTransfer) === null || _a === void 0 ? void 0 : _a.setData("application/json", JSON.stringify(dto));
     });
-});
-doingBox.addEventListener("dragover", function (event) {
-    event.preventDefault();
-    console.log("ELEMENT OVER A DROPZONE");
-});
-doingBox.addEventListener("drop", function (event) {
-    var _a;
-    const cardID = (_a = event.dataTransfer) === null || _a === void 0 ? void 0 : _a.getData("text/plain");
-    console.log("ELEMENT DROPPED IN THE DROPZONE OF ID", cardID);
-    this.appendChild(document.getElementById(cardID));
+    board.addEventListener("dragover", function (event) {
+        event.preventDefault();
+        console.log("ELEMENT OVER A DROPZONE");
+    });
+    board.addEventListener("drop", function (event) {
+        var _a;
+        const receivedDto = JSON.parse((_a = event.dataTransfer) === null || _a === void 0 ? void 0 : _a.getData("application/json"));
+        console.log("ELEMENT DROPPED IN THE DROPZONE OF ID", receivedDto);
+        if (parseInt(this.id) - receivedDto.boardId <= 1) {
+            this.appendChild(document.getElementById(receivedDto.targetElementId));
+        }
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,41 @@
-const boards = ["Backlog", "Doing", "Review", "Done"];
+const boards = ["Todo", "Doing", "Done"];
 
-const doingBox = document.querySelector(".doing") as HTMLDivElement;
+const allBoards = document.querySelectorAll<HTMLDivElement>(".board");
 const allCards = document.querySelectorAll<HTMLDivElement>(".card");
 
 allCards.forEach((card) => {
-  card.addEventListener("dragstart", function (event: DragEvent) {
+  //   card.addEventListener("dragstart", function (event: DragEvent) {
+  //     const targetElement = event.target as HTMLDivElement;
+  //     console.log("THE ELEMENT BEING DRAGGED IS ", targetElement.id);
+  //     event.dataTransfer?.setData("text/plain", targetElement.id);
+  //   });
+});
+
+allBoards.forEach(function (board) {
+  board.addEventListener("dragstart", function (event: DragEvent) {
     const targetElement = event.target as HTMLDivElement;
     console.log("THE ELEMENT BEING DRAGGED IS ", targetElement.id);
-    event.dataTransfer?.setData("text/plain", targetElement.id);
+    const dto = {
+      boardId: parseInt((<HTMLDivElement>event.currentTarget).id),
+      targetElementId: targetElement.id,
+    };
+    event.dataTransfer?.setData("application/json", JSON.stringify(dto));
   });
-});
 
-doingBox.addEventListener("dragover", function (event: DragEvent) {
-  event.preventDefault();
-  console.log("ELEMENT OVER A DROPZONE");
-});
+  board.addEventListener("dragover", function (event: DragEvent) {
+    event.preventDefault();
+    console.log("ELEMENT OVER A DROPZONE");
+  });
 
-doingBox.addEventListener("drop", function (event: DragEvent) {
-  const cardID = event.dataTransfer?.getData("text/plain")!;
-  console.log("ELEMENT DROPPED IN THE DROPZONE OF ID", cardID);
-  this.appendChild(document.getElementById(cardID) as HTMLDivElement);
+  board.addEventListener("drop", function (event: DragEvent) {
+    const receivedDto = JSON.parse(
+      event.dataTransfer?.getData("application/json")!
+    );
+    console.log("ELEMENT DROPPED IN THE DROPZONE OF ID", receivedDto);
+    if (parseInt(this.id) - receivedDto.boardId <= 1) {
+      this.appendChild(
+        document.getElementById(receivedDto.targetElementId) as HTMLDivElement
+      );
+    }
+  });
 });
